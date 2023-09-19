6cb88258048286ff1355e446d6b407b036883621
HEITOR SEIBERT DOS SANTOS <heitor.santos@alunos.sc.senac.br>
2023-09-18 21:55:08 -0300

    Get e Set Pessoas

---
+++ app/Controllers/Pessoas.php
@@ -0,0 +1,71 @@
+<?php
+use App\Controllers\BaseController;
+
+class Pessoas extends BaseController{
+    private $nome;
+    private $endereco;
+    private $bairro;
+    private $cidade;
+    private $estado;
+    private $cep;
+
+    public function __construct($nome, $endereco, $bairro, $cidade, $estado, $cep)
+    {
+        $this-> nome = $nome;
+        $this-> endereco = $endereco;
+        $this-> bairro = $bairro;
+        $this-> cidade = $cidade;
+        $this-> estado = $estado;
+        $this-> nome = $nome;
+
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setNome($nome){
+        $this-> nome = $nome;
+    }
+
+    public function getEndereco(){
+        return $this->endereco;
+    }
+
+    public function setEndereco($endereco){
+        $this-> endereco = $endereco;
+    }
+
+    public function getBairro(){
+        return $this-> bairro;
+    }
+
+    public function setBairro($bairro){
+        $this-> bairro = $bairro;
+    }
+
+    public function getCidade(){
+        return $this-> cidade;
+    }
+
+    public function setCidade($cidade){
+        $this-> cidade = $cidade;
+    }
+
+    public function getEstado(){
+        return $this-> estado;
+    }
+
+    public function setEstado($estado){
+        $this-> estado = $estado;
+    }
+
+    public function getCep(){
+        return $this-> cep;
+    }
+
+    public function setCep($cep){
+        $this-> cep = $cep;
+    }
+
+} 
